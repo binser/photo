1a8f7e5609c4e353489c9524d772175e6407edaa
binser <bins91@mail.ru>
2015-11-16 17:59:25 +0300

    [*] добавление lightbox

--- src/PhotoBundle/DataFixtures/ORM/LoadPhotoData.php
+++ src/PhotoBundle/DataFixtures/ORM/LoadPhotoData.php
@@ -32,10 +32,28 @@ class LoadPhotoData extends AbstractFixture implements OrderedFixtureInterface
         $photo4->setName('5ac4829e232eb024d9a9c154ad321fc6.jpg');
         $photo4->setAlbum($this->getReference('album-progulka'));
 
+        $photo5 = new Photo();
+        $photo5->setEnabled();
+        $photo5->setName('eb66159c445125841cf58c09113dc0a4.jpg');
+        $photo5->setAlbum($this->getReference('album-progulka'));
+
+        $photo6 = new Photo();
+        $photo6->setEnabled();
+        $photo6->setName('287bde35632cf8a742c96db8a1b33074.jpg');
+        $photo6->setAlbum($this->getReference('album-progulka'));
+
+        $photo7 = new Photo();
+        $photo7->setEnabled();
+        $photo7->setName('be8b7154ef263cf213f2a8dac6a5c70c.jpg');
+        $photo7->setAlbum($this->getReference('album-progulka'));
+
         $em->persist($photo1);
         $em->persist($photo2);
         $em->persist($photo3);
         $em->persist($photo4);
+        $em->persist($photo5);
+        $em->persist($photo6);
+        $em->persist($photo7);
         $em->flush();
     }
 
